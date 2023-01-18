986291ca9164ef7216d2a811ea48dcb2a9feda6a
JuanBarcelos <juanbarcelos@outlook.com>
2023-01-17 22:30:42 -0300

    feat: redisignier de home page

--- IBNC/Frontend/js/app.js
+++ IBNC/Frontend/js/app.js
@@ -1,10 +1,41 @@
+var swiper = new Swiper('.slide-container', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+});
+
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "./img/Menu.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "./img/Close.svg";
-    }
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = 'img/menu_white_36dp.svg';
+  } else {
+    menuMobile.classList.add('open');
+    document.querySelector('.icon').src = 'img/close_white_36dp.svg';
+  }
 }
